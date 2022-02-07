81ba728ab64038c63a9c9b77d2c9ab4a1352bf4e
deigo-lps <diego.lopes0909@gmail.com>
2022-02-07 16:20:12 -0300

    PUT

--- endpoints/usuario.php
+++ endpoints/usuario.php
@@ -40,6 +40,50 @@ function api_usuario_post($request){
 
   return rest_ensure_response($response);
 }
+function api_usuario_put($request){
+  $user = wp_get_current_user();
+  $user_id=$user->ID;
+
+  if($user_id > 0){
+    $email = sanitize_email($request['email']);
+    $senha = $request['senha'];
+    $nome = sanitize_text_field($request['nome']);
+    $rua = sanitize_text_field($request['rua']);
+    $cep = sanitize_text_field($request['cep']);
+    $numero = sanitize_text_field($request['numero']);
+    $bairro = sanitize_text_field($request['bairro']);
+    $cidade = sanitize_text_field($request['cidade']);
+    $estado = sanitize_text_field($request['estado']);
+
+    $email_exists = email_exists($email);
+
+    if(!$email_exists || $email_exists === $user_id){
+
+      $response=array(
+        'ID' => $user_id,
+        'user_pass' => $senha,
+        'user_email' => $email,
+        'display_name' => $nome,
+        'first_name' => $nome,
+      );
+
+      wp_update_user($response);
+
+      update_user_meta($user_id, 'rua', $rua);
+      update_user_meta($user_id, 'cep', $cep);
+      update_user_meta($user_id, 'numero', $numero);
+      update_user_meta($user_id, 'bairro', $bairro);
+      update_user_meta($user_id, 'cidade', $cidade);
+      update_user_meta($user_id, 'estado', $estado);
+    }
+    else{
+      $response = new WP_Error('email', 'Email já cadastrado.',array('status' => 403));
+    }
+} else{
+  $response = new WP_Error('permissao',array('status' => 401));
+}
+  return rest_ensure_response($response);
+}
 
 function api_usuario_get($request){
   $user=wp_get_current_user();
@@ -68,7 +112,8 @@ function api_usuario_get($request){
   return rest_ensure_response($response);
 }
 
-function registrar_api_usuario_post(){
+
+function registrar_api_usuario(){
   register_rest_route('api', '/usuario', array(
     array(
       'methods' => 'POST',
@@ -81,7 +126,12 @@ function registrar_api_usuario_post(){
       'callback' => 'api_usuario_get',
     ),
   ));
+  register_rest_route('api', '/usuario', array(
+    array(
+      'methods' => 'PUT',
+      'callback' => 'api_usuario_put',
+    ),
+  ));
 };
 
-add_action('rest_api_init', 'registrar_api_usuario_post');
-?>
+add_action('rest_api_init', 'registrar_api_usuario');
